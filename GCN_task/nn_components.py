9dbf21a0420ae7b94d75773ec0f027fc3d4d392c
Srivamshi Pittala <30509630+vamships@users.noreply.github.com>
2020-01-29 22:06:01 -0500

    Add files via upload

---
+++ GCN_task/nn_components.py
@@ -0,0 +1,170 @@
+import numpy as np
+import tensorflow as tf
+
+__all__ = [
+    "ann",
+    "node_average",
+    "attend_on_other",
+    "attend_on_other_recurrent",
+    "average_predictions",
+    "initializer",
+    "nonlinearity",
+]
+
+""" ====== Layers ====== """
+""" All layers have as first two parameters:
+        - input: input tensor or tuple of input tensors
+        - params: dictionary of parameters, could be None
+"""
+
+def ann(input, params, out_dims=None, dropout_keep_prob=1.0, trainable=True, **kwargs):
+    input = tf.nn.dropout(input, dropout_keep_prob)
+    in_dims = input.get_shape()[-1].value
+    out_dims = in_dims if out_dims is None else out_dims
+    if params is None:
+        W = tf.Variable(initializer("he", [in_dims, out_dims]), name="w", trainable=trainable)
+        b = tf.Variable(initializer("zero", [out_dims]), name="b", trainable=trainable)
+        params = {"W": W, "b": b}
+    else:
+        W, b = params["W"], params["b"]
+    Z = tf.matmul(input, W) + b
+
+    regularizer = tf.constant(0.0)
+    
+    return Z, params, regularizer
+
+def node_average(input, params, filters=None, dropout_keep_prob=1.0, trainable=True, **kwargs):
+    vertices, edges, nh_indices = input
+    nh_indices = tf.squeeze(nh_indices, axis=2)
+    v_shape = vertices.get_shape()
+    #print("In node_average: %d" % (v_shape[1].value))
+    nh_sizes = tf.expand_dims(tf.count_nonzero(nh_indices + 1, axis=1, dtype=tf.float32), -1)  # for fixed number of neighbors, -1 is a pad value
+
+    if params is None:
+        # create new weights
+        Wc = tf.Variable(initializer("he", (v_shape[1].value, filters)), name="Wc", trainable=trainable)  # (v_dims, filters)
+        Wn = tf.Variable(initializer("he", (v_shape[1].value, filters)), name="Wn", trainable=trainable)  # (v_dims, filters)
+        b = tf.Variable(initializer("zero", (filters,)), name="b", trainable=trainable)
+    else:
+        Wn, Wc = params["Wn"], params["Wc"]
+        filters = Wc.get_shape()[-1].value
+        b = params["b"]
+    params = {"Wn": Wn, "Wc": Wc, "b": b}
+
+    # generate vertex signals
+    Zc = tf.matmul(vertices, Wc, name="Zc")  # (n_verts, filters)
+    # create neighbor signals
+    v_Wn = tf.matmul(vertices, Wn, name="v_Wn")  # (n_verts, filters)
+    Zn = tf.divide(tf.reduce_sum(tf.gather(v_Wn, nh_indices), 1),
+                   tf.maximum(nh_sizes, tf.ones_like(nh_sizes)))  # (n_verts, v_filters)
+
+    nonlin = nonlinearity("relu")
+    sig = Zn + Zc + b
+    h = tf.reshape(nonlin(sig), tf.constant([-1, filters]))
+    h = tf.nn.dropout(h, dropout_keep_prob)
+    return h, params
+
+def attend_on_other(input, params, filters=None, dropout_keep_prob=1.0, trainable=True, **kwargs):
+    input1, input2, pairs = input
+
+    num_resid_l = tf.reduce_max(pairs[:,0])+1
+    num_resid_r = tf.reduce_max(pairs[:,1])+1
+
+    in_shape = input1.get_shape()
+    rec_shape = input2.get_shape()
+
+    if params is None:
+        # create new weights
+        Wc = tf.Variable(initializer("he", (in_shape[1].value, in_shape[1].value)), name="Wc", trainable=trainable)  # (v_dims, filters)
+    else:
+        Wc = params["Wc"]
+        filters = Wc.get_shape()[-1].value
+    params = {"Wc": Wc}
+
+    # generate vertex signals
+    Zc_intermediate_pre = tf.matmul(input1, Wc, name="Zc_temp")  # (n_verts, filters)
+    Zc_intermediate_post = tf.matmul(tf.transpose(Wc),tf.transpose(input2), name="Zc_temp")
+    sig = tf.matmul(Zc_intermediate_pre, Zc_intermediate_post, name="sig")
+
+    h = tf.nn.relu(sig)
+
+    h_flat = tf.reshape(h, [-1])
+
+    h_mean, h_var = tf.nn.moments(h_flat, axes=0)
+    #h_flat_norm = h_flat
+    h_flat_norm = tf.nn.l2_normalize(h_flat)
+    #h_flat_norm = (h_flat - h_mean)/h_var
+
+    h_norm = tf.reshape(h_flat_norm, [num_resid_l, -1])
+
+    h_softmax = h_norm
+
+    feats_other = tf.transpose(tf.matmul(input2,h_softmax,transpose_a=True,transpose_b=True))
+
+    return feats_other, h, h_softmax, params
+
+def attend_on_other_recurrent(input, params, filters=None, dropout_keep_prob=1.0, trainable=True, **kwargs):
+    input1, input2, pairs, preds_semi = input
+
+    num_resid_l = tf.reduce_max(pairs[:,0])+1
+    num_resid_r = tf.reduce_max(pairs[:,1])+1
+
+    in_shape = input1.get_shape()
+    rec_shape = input2.get_shape()
+
+    if params is None:
+        # create new weights
+        Wc = tf.Variable(initializer("he", (in_shape[1].value, in_shape[1].value)), name="Wc", trainable=trainable)  # (v_dims, filters)
+    else:
+        Wc = params["Wc"]
+        filters = Wc.get_shape()[-1].value
+    params = {"Wc": Wc}
+
+    # generate vertex signals
+    Zc_intermediate_pre = tf.matmul(input1, Wc, name="Zc_temp")  # (n_verts, filters)
+    Zc_intermediate_post = tf.matmul(tf.transpose(Wc),tf.transpose(input2), name="Zc_temp")
+    sig = tf.matmul(Zc_intermediate_pre, Zc_intermediate_post, name="sig")
+
+    h = tf.nn.relu(sig)
+
+    h_scaled = h * preds_semi[:,0]
+
+    h_flat = tf.reshape(h_scaled, [-1, 1])
+
+    h_flat_norm = tf.nn.l2_normalize(h_flat)
+    h_norm = tf.reshape(h_flat_norm, [num_resid_l, -1])
+
+    h_softmax = h_norm
+    #h_softmax = labels_attn
+
+    feats_other = tf.transpose(tf.matmul(input2,h_softmax,transpose_a=True,transpose_b=True))
+
+    return feats_other, h_scaled, h_softmax, params
+
+
+def average_predictions(input, _, **kwargs):
+    combined = tf.reduce_mean(tf.stack(tf.split(input, 2)), 0)
+    return combined, None
+
+
+""" ======== Non Layers ========= """
+
+
+def initializer(init, shape):
+    if init == "zero":
+        return tf.zeros(shape)
+    elif init == "he":
+        fan_in = np.prod(shape[0:-1])
+        std = 1/np.sqrt(fan_in)
+        return tf.random_uniform(shape, minval=-std, maxval=std)
+
+
+def nonlinearity(nl):
+    if nl == "relu":
+        return tf.nn.relu
+    elif nl == "leaky_relu":
+        return tf.nn.leaky_relu
+    elif nl == "tanh":
+        return tf.nn.tanh
+    elif nl == "linear" or nl == "none":
+        return lambda x: x
---
+++ GCN_task/sample_experiment_conv2.py
@@ -0,0 +1,141 @@
+import os
+import sys
+import yaml
+import traceback
+import cPickle
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from configuration import mode_experiment, data_directory, experiment_directory, output_directory, printt
+from train_test import TrainTest
+from sample_pw_classifier_conv2 import PWClassifier
+from results_processor import ResultsProcessor
+
+## Random Seeds
+# each random seed represents an experimental replication.
+# You can add or remove list elements to change the number
+# of replications for an experiment.
+seeds = [
+    {"tf_seed": 649737, "np_seed": 29820},
+    {"tf_seed": 395408, "np_seed": 185228},
+    {"tf_seed": 252356, "np_seed": 703889},
+    {"tf_seed": 343053, "np_seed": 999360},
+    {"tf_seed": 743746, "np_seed": 67440}
+]
+
+# Load experiment specified in system args
+exp_file = "node_edge_conv2.yml"
+printt("Running Experiment File: {}".format(exp_file))
+f_name = exp_file.split(".")[0] if "." in exp_file else exp_file
+exp_specs = yaml.load(open(os.path.join(experiment_directory, exp_file), 'r').read())
+
+# setup output directory
+outdir = os.path.join(output_directory, f_name)
+if not os.path.exists(outdir):
+    os.mkdir(outdir)
+results_processor = ResultsProcessor()
+
+# create results log
+results_log = os.path.join(outdir, "results.csv")
+with open(results_log, 'w') as f:
+    f.write("")
+
+# write experiment specifications to file
+with open(os.path.join(outdir, "experiment.yml"), 'w') as f:
+    f.write("{}\n".format(yaml.dump(exp_specs)))
+
+# pre-process to include residue indices
+def preProcessData(data):
+    num_complex = len(data)
+    for idx_complex in range(num_complex):
+
+        # Use only up to 15 neighbors during convolution
+        data[idx_complex]["l_hood_indices"] = data[idx_complex]["l_hood_indices"][:,:15,:]
+        data[idx_complex]["r_hood_indices"] = data[idx_complex]["r_hood_indices"][:,:15,:]
+
+        data[idx_complex]["l_edge"] = data[idx_complex]["l_edge"][:,:15,:]
+        data[idx_complex]["r_edge"] = data[idx_complex]["r_edge"][:,:15,:]
+
+# Remove antigens as primary protein
+def remove_ags(data):
+    printt("Removing Ags")
+    new_data = []
+    for data_idx in range(len(data)):
+        if data_idx % 2 == 1:
+            new_data.append(data[data_idx])
+    return new_data
+
+# Remove antibodies as primary protein
+def remove_abs(data):
+    printt("Removing Abs")
+    new_data = []
+    for data_idx in range(len(data)):
+        if data_idx % 2 == 0:
+            new_data.append(data[data_idx])
+    return new_data
+
+first_experiment = True
+for name, experiment in exp_specs["experiments"]:
+    train_data_file = os.path.join(data_directory, experiment["train_data_file"])
+    val_data_file = os.path.join(data_directory, experiment["val_data_file"])
+    test_data_file = os.path.join(data_directory, experiment["test_data_file"])
+    try:
+        print("*" * 30)
+        printt("{} network".format(name))
+        printt("Loading train data")
+        train_data = cPickle.load(open(train_data_file))
+        preProcessData(train_data)
+
+        printt("Loading validation data")
+        val_data = cPickle.load(open(val_data_file))
+        preProcessData(val_data)
+
+        printt("Loading test data")
+        test_data = cPickle.load(open(test_data_file))
+        preProcessData(test_data)
+
+        # Prepare data as per task
+        if mode_experiment=="epitope":
+            train_data = remove_abs(train_data)
+            val_data = remove_abs(val_data)
+            test_data = remove_abs(test_data)
+        else:
+            train_data = remove_ags(train_data)
+            val_data = remove_ags(val_data)
+            test_data = remove_ags(test_data)
+
+        print(len(train_data), len(val_data), len(test_data))
+
+        data = {"train": train_data, "val": val_data, "test": test_data}
+        #data = {"train": val_data, "val": train_data, "test": test_data}
+        for i, seed_pair in enumerate(seeds):
+            printt("{}: rep{}".format(name, i))
+            # set tensorflow and numpy seeds
+            tf.set_random_seed(seed_pair["tf_seed"])
+            np.random.seed(int(seed_pair["np_seed"]))
+            
+            # build model
+            printt("Building model")
+            learning_rate = 0.001
+            model = PWClassifier(experiment["layers"], experiment["layer_args"], data["train"], learning_rate, 0.1, outdir)
+            
+            # train and test the model
+            headers, results = TrainTest(results_processor).fit_model_and_activate(exp_specs, data, model, experiment["layers"], i, outdir)
+
+            # write headers to file if haven't already
+            if first_experiment:
+                with open(results_log, 'a') as f:
+                    f.write("{}\n".format(",".join(["file", "experiment", "rep"] + headers)))
+                first_experiment = False
+            
+            # write results to file
+            with open(results_log, 'a') as f:
+                f.write("{}, {}, {}, {}\n".format(f_name, name, i, ",".join([str(r) for r in results])))
+            print("*" * 30)
+
+    except Exception as er:
+        if er is KeyboardInterrupt:
+            raise er
+        ex_str = traceback.format_exc()
+        printt(ex_str)
+        printt("Experiment failed: {}".format(exp_specs))
